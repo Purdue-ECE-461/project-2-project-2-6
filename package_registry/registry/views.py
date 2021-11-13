eb381dd1309fa9250d7fb002001223c3cdb6f36f
Benjamin Castro <castro.bafc@outlook.com>
2021-11-13 14:10:42 -0500

    adding print statements for debugging

--- package_registry/registry/views.py
+++ package_registry/registry/views.py
@@ -29,10 +29,12 @@ def apiOverview(request):
 
 @api_view(['GET'])
 def packages_middleware(request):
-    body_unicode = request.body.decode('utf-8')
-    body_data = json.loads(body_unicode)
-    print('Type of body_data: {data}'.format(data=type(body_data)))
-    print('body_data: {dato}'.format(dato=body_data))
+    print(request.body)
+    print(request.body.decode('utf-8'))
+    # body_unicode = request.body.decode('utf-8')
+    # body_data = json.loads(body_unicode)
+    # print('Type of body_data: {data}'.format(data=type(body_data)))
+    # print('body_data: {dato}'.format(dato=body_data))
 
     # determine offset query parameter
     offset = request.GET.get('Offset')
@@ -45,8 +47,9 @@ def packages_middleware(request):
     response = []
     print('AAA')
     body_unicode = request.body.decode('utf-8')
-    body_data = json.loads(body_unicode)
     print('BBB')
+    body_data = json.loads(body_unicode)
+    print('CCC')
 
     if len(body_data) < 1:
         return Response({'message: empty request body array'}, status=400)
