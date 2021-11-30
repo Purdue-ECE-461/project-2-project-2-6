505be65e9e0b9f59c1efbb883457833c7014b3fc
Steven Rutledge <stevenrrutledge@gmail.com>
2021-11-30 13:05:24 -0500

    update tests

--- package_registry/tests/test1.py
+++ package_registry/tests/test1.py
@@ -1,14 +1,9 @@
 # unit test case
-import unittest
+
+def test_sample(self):
+    testValue = True
+    # error message in case if test case got failed
+    message = "Test value is not true."
+    # assertTrue() to check true of test value
+    self.assertTrue( testValue, message)
  
-class TestStringMethods(unittest.TestCase):
-    # test function
-    def test_samplee(self):
-        testValue = True
-        # error message in case if test case got failed
-        message = "Test value is not true."
-        # assertTrue() to check true of test value
-        self.assertTrue( testValue, message)
- 
-if __name__ == '__main__':
-    unittest.main()
