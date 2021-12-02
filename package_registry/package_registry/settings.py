5d1348f07feba9fe50a65ee3ad2a0d294f721c53
castroben <76528046+castroben@users.noreply.github.com>
2021-12-02 11:39:17 -0500

    Update settings.py

--- package_registry/package_registry/settings.py
+++ package_registry/package_registry/settings.py
@@ -1,9 +1,5 @@
 from pathlib import Path
 
-# Setting up local environment
-# env = environ.Env()
-# environ.Env.read_env()
-# SECRET_KEY = env('SECRET')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
