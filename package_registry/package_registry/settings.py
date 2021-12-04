7b3c2be485d0165908d00888a3da54754d5da603
Steven Rutledge <54333715+srutledge12@users.noreply.github.com>
2021-12-04 16:34:21 -0500

    Update settings.py

--- package_registry/package_registry/settings.py
+++ package_registry/package_registry/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-g2=e&!si45va)z=_vvk!h(xs!9r=cnzktz$pt-hpc3+s$2g##o'
-const PROD_DATABASE_PASSWORD2 = os.environ.get('PROD_DATABASE_PASSWORD2')
+PROD_DATABASE_PASSWORD2 = os.environ.get('PROD_DATABASE_PASSWORD2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
