a68a0dc9ef0058194fc11bee859d4f06e3bd5601
Benjamin Castro <castro.bafc@outlook.com>
2021-12-05 18:38:00 -0500

    updating postman test suite and url paths

--- package_registry/registry/urls.py
+++ package_registry/registry/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview),
-    path('packages/', views.packages_middleware, name='packages_middleware'),
+    path('packages', views.packages_middleware, name='packages_middleware'),
     path('package/<str:pk>', views.package_middleware, name='package'),
-    path('package/', views.create_package_middleware, name='create'),
+    path('package', views.create_package_middleware, name='create'),
     path('package/byName/<str:name>', views.byName_middleware, name='byName'),
-    path('reset/', views.reset_middleware, name='reset'),
-    path('authenticate/', views.create_token_middleware, name='authentication'),
+    path('reset', views.reset_middleware, name='reset'),
+    path('authenticate', views.create_token_middleware, name='authentication'),
     path('package/<str:pk>/rate', views.rate_package, name='rate')
 ]
